61f1406980ba63a6dffea5ee616213c45f0886b7
Cosmo <johan.amador200@gmail.com>
2024-11-05 16:44:21 -0500

    ServiceApplication.java

---
+++ odipartrack-service/src/main/java/com/odipartrack/OdipartrackServiceApplication.java
@@ -0,0 +1,11 @@
+package com.odipartrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdipartrackServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OdipartrackServiceApplication.class, args);
+    }
+}
